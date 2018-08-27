f8c0c59abb2e2b829e58ed080ca4bb7110cec135
max <40814622+vloxomire@users.noreply.github.com>
2018-08-27 10:25:53 -0300

    1025

---
+++ app/src/main/java/com/max/proyecto02/Proyecto02Activity.java
@@ -0,0 +1,13 @@
+package com.max.proyecto02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Proyecto02Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.proyecto02);
+    }
+}
